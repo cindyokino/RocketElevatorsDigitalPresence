ad1a094084777acaada451f3de517eb17946cccb
Cindy Okino <cindy.okino@gmail.com>
2020-09-09 17:25:56 -0400

    delete css files, add dropdown to navbar

---
+++ rocket-elevators-project/HTML_BS4/assets/js/quote-form.js
@@ -0,0 +1,15 @@
+/** ********************************************** **
+	@Author			Cindy Okino
+	@Website		https://github.com/cindyokino
+	@Last Update	September 16, 2020
+
+
+	TABLE CONTENTS
+	-------------------------------
+
+
+	INLINE SCRIPTS
+	-------------------------------
+
+*************************************************** **/
+
