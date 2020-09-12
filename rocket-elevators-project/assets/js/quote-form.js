c98e490dac449fa4252b8f4eb0a3913f4a28fbc6
Cindy Okino <cindy.okino@gmail.com>
2020-09-11 20:46:12 -0400

    select radio button for elevator line

--- rocket-elevators-project/assets/js/quote-form.js
+++ rocket-elevators-project/assets/js/quote-form.js
@@ -4,20 +4,53 @@
 	@Last Update	September 16, 2020
 
 
-	TABLE CONTENTS
+	TABLE CONTENTS 
 	-------------------------------
+		GLOBAL VARIABLES AND CONSTANTS
+		-------------------------------
+		FUNCTIONS
 
-
-	INLINE SCRIPTS
+			CALCULATE NUMBER OF ELEVATORS - RESIDENTIAL BUILDING
+			CALCULATE NUMBER OF ELEVATORS - COMMERCIAL BUILDING
+			CALCULATE NUMBER OF ELEVATORS - CORPORATE & HYBRID BUILDINGS
+			CALCULATE NUMBER OF ELEVATORS - CORPORATE BUILDING - call function numElevatorsCorporateAndHybrid()
+			CALCULATE NUMBER OF ELEVATORS - HYBRID BUILDING  - call function numElevatorsCorporateAndHybrid()
+			RADIO BUTTONS - SHOW & HIDE FORMS AND ITS BUTTONS
+			CALCULATE PRICE EQUIPMENTS
+			CALCULATE PRICE BY LINE OF PRODUCT SELECTED
+		-------------------------------
+		SCRIPTS
+		-------------------------------		
 	-------------------------------
 
 *************************************************** **/
 
+
+
+
+/* ------------------------------------------------------------------------------
+======================== GLOBAL VARIABLES AND CONSTANTS ======================== 
+--------------------------------------------------------------------------------*/
+
 var numElevators = 0;
 var priceElevators = 0;
 var priceInstallation = 0;
+var totalPrice = 0;
+
+const standardPrice = 7565;
+const premiumPrice = 12345;
+const exceliumPrice =15400;
+
+const standardFee= 10;
+const premiumFee = 13;
+const exceliumFee = 16;
 
 
+
+
+/* ------------------------------------------------------------------------------
+================================== FUNCTIONS ================================== 
+--------------------------------------------------------------------------------*/
 
 /***** CALCULATE NUMBER OF ELEVATORS - RESIDENTIAL BUILDING *****/
 var numElevatorsResidential = function () {
@@ -86,14 +119,7 @@ var numElevatorsCorporate = function () {
 /***** CALCULATE NUMBER OF ELEVATORS - HYBRID BUILDING  - call function numElevatorsCorporateAndHybrid() *****/
 var numElevatorsHybrid = function () {
 	numElevatorsCorporateAndHybrid();
-}
-
-
-
-// /***** CALCULATE PRICE BY LINE OF  *****/
-// var calculatePricesByLine = function () {
-// 	$("#calculatedNumOfElevators").text(elevators);	
-// }
+};
 
 
 
@@ -110,9 +136,113 @@ var onChangeBuildingTypeform = function () {
 	$("#" + optionNameButton).show();
 
 	numElevators = 0;
+
 	$("#calculatedNumOfElevators").text(numElevators);
 };
 
+
+//========================================================================================================================================
+/***** FORMAT MONEY - JAVASCRIPT NUMBER FORMATTER *****/
+var formatter = new Intl.NumberFormat(undefined, {
+	style: 'currency',
+	currency: 'USD',
+});
+  
+// formatter.format(2500); EXAMPLE
+
+
+//========================================================================================================================================
+// /***** CALCULATE PRICES *****/
+// var calculatePrices = function () {
+	// 	priceElevators = numElevators * optionPrice;
+	// 	priceInstallation = priceElevators * (optionFee / 100);
+	// 	totalPrice = priceElevators + priceInstallation;
+// }
+
+/***** CALCULATE FEES *****/
+
+/***** CALCULATE TOTAL PRICES *****/
+
+/***** CALCULATE PRICES STANDARD *****/
+var standardCalculedPrices = function () {
+	priceElevators = numElevators * standardPrice;
+	priceInstallation = price * (standardFee / 100);
+	totalPrice = price + fees;
+
+	// $("#feeCost").text(priceInstallation);
+	// $("#calculatedTotalPrice").text(totalPrice);
+};
+
+// /***** CALCULATE PRICES STANDARD *****/
+// var premiumCalculedPrices = function () {
+// };
+
+// /***** CALCULATE PRICES STANDARD *****/
+// var exceliumCalculedPrices = function () {
+// };
+
+
+//==============================================================================================================================
+// /***** CALCULATE PRICE BY LINE OF PRODUCT SELECTED *****/
+// var onChangeProductLine = function () {
+// 	// priceElevators = 0;
+// 	// priceInstallation = 0;
+// 	// totalPrice = 0;
+// 	// $("#feeCost").text(priceInstallation);
+// 	// $("#calculatedTotalPrice").text(totalPrice);
+
+// 	var optionName = this.id;
+// 	optionName = optionName.replace("Line", "");
+
+// 	var optionPrice = (optionName + "Price");
+// 	var optionFee = optionName + "Fee";
+
+// 	// priceElevators	= numElevators * optionPrice;
+// 	// priceInstallation = priceElevators * (optionFee / 100);
+// 	// totalPrice = priceElevators + priceInstallation;
+
+// 	standardCalculedPrices(optionPrice, optionFee);
+
+// 	$("#feeCost").text(priceInstallation);
+// 	$("#calculatedTotalPrice").text(totalPrice);
+// };
+//==============================================================================================================================
+/***** CALCULATE PRICE BY LINE OF PRODUCT SELECTED *****/
+var onChangeProductLine = function () {
+	priceElevators = 0;
+	priceInstallation = 0;
+	totalPrice = 0;
+	$("#feeCost").text(formatter.format(priceInstallation));
+	$("#calculatedTotalPrice").text(formatter.format(totalPrice));
+
+	var optionName = this.id;
+	optionName = optionName.replace("Line", "");
+	var optionPrice = optionName + "Price";
+	var optionFee = optionName + "Fee";
+
+	priceElevators	= numElevators * optionPrice;
+	calculatedFee = priceElevators * (optionFee / 100);
+	totalPrice = priceElevators + priceInstallation;
+
+	if ($("standardLine").checked) {
+		standardCalculedPrices();
+	}
+
+	$("#chosenLine").text(optionName);
+	$("#unitPrice").text(formatter.format(optionPrice));
+	$("#feeCost").text(formatter.format(calculatedFee));
+	$("#calculatedTotalPrice").text(formatter.format(totalPrice));
+};
+//==============================================================================================================================
+
+
+
+
+/* ------------------------------------------------------------------------------
+=================================== SCRIPTS ===================================
+--------------------------------------------------------------------------------*/
+
+/***** RADIO BUTTONS - SHOW & HIDE FORMS AND ITS BUTTONS *****/
 $("input[name=typeOfBuilding]").on("change", onChangeBuildingTypeform); 
 
 $(document).ready(function () {
@@ -121,12 +251,26 @@ $(document).ready(function () {
 
 
 
+/***** RADIO BUTTONS - SELECT PRODUCT LINE *****/
+$("input[name=productLine]").on("change", onChangeProductLine); 
+
+$(document).ready(function () {
+	$("#standardLine").change();
+});
 
 
+//TENTANDO FAZER O ENTAR FUNCIONAR COMO UM BUTTON.CLICK() ****************** DELETAR SE NAO ARRUMAMR!!!
+// var input = $("input[type=number]");
+// input.addEventListener("keyup", function(event) {
+//   if (event.keyCode === 13) {
+//    event.preventDefault();
+//    $("#quote-form button").click();
+//   }
+// });
 
 
-
-// $("#termsAndConditions").click(function(){  DELETE THIS after implement the footer
+//****************** DELETAR SE NAO ARRUMAMR!!! sobre alerts no footer!!
+// $("#termsAndConditions").click(function(){  DELETE THIS after implement the footer 
 // 	var str = "I accept all risks using elevators";
 // 	alert(str);
 // });
